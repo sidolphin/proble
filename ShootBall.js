c86b187bd40bca28dd6edaf9881aae5b9adf5419
sidolphin <62950778+sidolphin@users.noreply.github.com>
2020-04-30 16:02:00 +0530

    Add files via upload

---
+++ ShootBall.js
@@ -0,0 +1,32 @@
+class ShootBall{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB;
+
+        this.ball = Constraint.create(options);
+        World.add(world, this.ball);
+    }
+
+fly(){
+    this.ball.bodyA=null;
+}
+
+    }
+
+
+    display(){
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+                
+    }
+
+}
